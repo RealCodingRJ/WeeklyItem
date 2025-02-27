a8ca457278654cd4a8aaa55557f30f291c6f6640
Ryan James <realcoder2023@gmail.com>
2025-02-27 11:51:50 -0500

    Added Calculations

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import itemCalculated.CalculationsKt;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println("Enter Item Price: ");
+        float item = scan.nextFloat();
+
+        System.out.println("Enter Your Hourly Wage: ");
+        float wage = scan.nextFloat();
+
+        System.out.println("Enter Days You Work: ");
+        float days = scan.nextFloat();
+
+        System.out.println("Enter Hours You Work: ");
+        float hours = scan.nextFloat();
+
+        float cal = CalculationsKt.calculate(wage, days, hours);
+        float itemCalculate = (int)(int)item / cal;
+
+        System.out.println("You Will Have to Wait: "
+                + (int)(int)itemCalculate + " Weeks for Item That is: $" + item );
+
+    }
+}
---
+++ src/itemCalculated/Calculations.kt
@@ -0,0 +1,6 @@
+﻿package itemCalculated;
+
+fun calculate(a:Float, b:Float, c:Float): Float {
+    return a * b * c;
+
+}
